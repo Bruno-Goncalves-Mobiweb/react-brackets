c0ec4323699a24ee2bdadc9ebbfa71c093201759
Bruno <bruno.goncalves@mobiweb.pt>
2024-02-28 20:26:42 +0000

    solve problem

--- dist/brackets/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { ISingleEliminationProps } from "../types/SingleElimination";
-declare const SingleElimination: ({ rounds, rtl, roundClassName, bracketClassName, mobileBreakpoint, renderSeedComponent, roundTitleComponent, }: ISingleEliminationProps) => React.JSX.Element;
-export { SingleElimination };
--- dist/components/seed.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { IMobileProps } from '../types/Seed';
-export declare const SeedItem: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>>;
-export declare const SeedTeam: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>>;
-export declare const SeedTime: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, IMobileProps>>;
-export declare const SingleLineSeed: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, IMobileProps>>;
-export declare const Seed: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, IMobileProps>>;
