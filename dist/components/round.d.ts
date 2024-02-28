c3188c1647c6dde61581f207437faea27f2d0c7e
Bruno <bruno.goncalves@mobiweb.pt>
2024-02-28 20:17:06 +0000

    tentativa numero dois

---
+++ dist/components/round.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+import { IMobileProps } from '../types/Seed';
+export declare const Bracket: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, IMobileProps>>;
+export declare const Round: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").Substitute<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, IMobileProps>>;
+export declare const RoundTitle: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>>;
+export declare const SeedsList: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>>;
